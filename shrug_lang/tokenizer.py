954228968e7638add87121d8e4f10a66c42798ca
Benjamin Wu <benjaminwu124@gmail.com>
2018-09-18 15:54:22 -0400

    Handle strings containing spaces

--- shrug_lang/tokenizer.py
+++ shrug_lang/tokenizer.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 
 class Tokens(Enum):
@@ -31,15 +32,40 @@ class LineParser:
     def parse(self, line: str):
         if len(line) == 0:
             return [(Tokens.EOL,)]
-        unparsed_tokens = line.split(' ')
-        tokens = list(filter(
-            None, [self.parse_token(unparsed) for unparsed in unparsed_tokens]
-        ))
+        unparsed_tokens = filter(None, self.join_strings(line.split(' ')))
+        tokens = [self.parse_token(unparsed) for unparsed in unparsed_tokens]
         tokens.append((Tokens.EOL,))
         return tokens
 
     @staticmethod
+    def join_strings(unparsed_tokens: List[str]):
+        """Join strings that have spaces in them"""
+        new_unparsed = []
+        reading_string = False
+        current_string = []
+        for unparsed_token in unparsed_tokens:
+            if reading_string:
+                if unparsed_token.endswith('"'):
+                    reading_string = False
+                    current_string.append(unparsed_token)
+                    new_unparsed.append(' '.join(current_string))
+                    current_string.clear()
+                else:
+                    current_string.append(unparsed_token)
+            elif (unparsed_token.startswith('"') and
+                  (len(unparsed_token) == 1 or
+                   not unparsed_token.endswith('"'))):
+                reading_string = True
+                current_string.append(unparsed_token)
+            else:
+                new_unparsed.append(unparsed_token)
+        if len(current_string):
+            new_unparsed.append(' '.join(current_string))
+        return new_unparsed
+
+    @staticmethod
     def parse_token(unparsed_token: str):
+        """Get token from given string"""
         if unparsed_token == '':
             return None
         if unparsed_token == '¯\_(ツ)_/¯':
