518d20754bd64c24a140bd9205597e2265e568e2
Benjamin Wu <benjaminwu124@gmail.com>
2018-09-18 17:58:11 -0400

    Rename LineParser to Tokenizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,67 +1,67 @@
 import unittest
 
-from shrug_lang.tokenizer import LineParser, Tokens
+from shrug_lang.tokenizer import Tokenizer, Tokens
 
 
 class TestLineParser(unittest.TestCase):
     """Line parser class should return correctly read tokens"""
     def setUp(self):
-        self.parser = LineParser()
+        self.parser = Tokenizer()
 
     def test_empty_line(self):
         line = ''
         empty_line = [(Tokens.EOL,)]
-        self.assertEqual(empty_line, self.parser.parse(line))
+        self.assertEqual(empty_line, self.parser.parse_line(line))
 
         line = '     '
-        self.assertEqual(empty_line, self.parser.parse(line))
+        self.assertEqual(empty_line, self.parser.parse_line(line))
 
     def test_shrug(self):
         line = '¯\_(ツ)_/¯'
         single_shrug = [(Tokens.SHRUG,), (Tokens.EOL,)]
-        self.assertEqual(single_shrug, self.parser.parse(line))
+        self.assertEqual(single_shrug, self.parser.parse_line(line))
 
     def test_numbers(self):
         line = '2'
         single_number1 = [(Tokens.NUMBER, 2), (Tokens.EOL,)]
-        self.assertEqual(single_number1, self.parser.parse(line))
+        self.assertEqual(single_number1, self.parser.parse_line(line))
 
         line = '43435356'
         single_number2 = [(Tokens.NUMBER, 43435356), (Tokens.EOL,)]
-        self.assertEqual(single_number2, self.parser.parse(line))
+        self.assertEqual(single_number2, self.parser.parse_line(line))
 
         line = 'dfs3'
         invalid_id1 = [(Tokens.INVALID, 'dfs3'), (Tokens.EOL,)]
-        self.assertEqual(invalid_id1, self.parser.parse(line))
+        self.assertEqual(invalid_id1, self.parser.parse_line(line))
 
         line = 'abc%'
         invalid_id2 = [(Tokens.INVALID, 'abc%'), (Tokens.EOL,)]
-        self.assertEqual(invalid_id2, self.parser.parse(line))
+        self.assertEqual(invalid_id2, self.parser.parse_line(line))
 
     def test_ids(self):
         line = 'a'
         single_id1 = [(Tokens.ID, 'a'), (Tokens.EOL,)]
-        self.assertEqual(single_id1, self.parser.parse(line))
+        self.assertEqual(single_id1, self.parser.parse_line(line))
 
         line = 'variable'
         single_id2 = [(Tokens.ID, 'variable'), (Tokens.EOL,)]
-        self.assertEqual(single_id2, self.parser.parse(line))
+        self.assertEqual(single_id2, self.parser.parse_line(line))
 
     def test_strings(self):
         line = '""'
         empty_string = [(Tokens.STRING, ''), (Tokens.EOL,)]
-        self.assertEqual(empty_string, self.parser.parse(line))
+        self.assertEqual(empty_string, self.parser.parse_line(line))
 
         line = '"a"'
         short_string = [(Tokens.STRING, 'a'), (Tokens.EOL,)]
-        self.assertEqual(short_string, self.parser.parse(line))
+        self.assertEqual(short_string, self.parser.parse_line(line))
 
         line = '"a bc def gh i jk lmn op q rs tuv wx u y z"'
         long_string = [
             (Tokens.STRING, 'a bc def gh i jk lmn op q rs tuv wx u y z'),
             (Tokens.EOL,)
         ]
-        self.assertEqual(long_string, self.parser.parse(line))
+        self.assertEqual(long_string, self.parser.parse_line(line))
 
         line = '"fs\'d\' f" " s " " fd_ f"'
         weird_string = [
@@ -70,15 +70,15 @@ class TestLineParser(unittest.TestCase):
             (Tokens.STRING, ' fd_ f'),
             (Tokens.EOL,)
         ]
-        self.assertEqual(weird_string, self.parser.parse(line))
+        self.assertEqual(weird_string, self.parser.parse_line(line))
 
         line = '"a'
         invalid_string = [(Tokens.INVALID, '"a'), (Tokens.EOL,)]
-        self.assertEqual(invalid_string, self.parser.parse(line))
+        self.assertEqual(invalid_string, self.parser.parse_line(line))
 
         line = '"¯\_(ツ)_/¯"'
         shrug_string = [(Tokens.STRING, '¯\_(ツ)_/¯'), (Tokens.EOL,)]
-        self.assertEqual(shrug_string, self.parser.parse(line))
+        self.assertEqual(shrug_string, self.parser.parse_line(line))
 
     def test_complex(self):
         line = ('¯\_(ツ)_/¯ n ¯\_(ツ)_/¯ x ¯\_(ツ)_/¯ ¯\_(ツ)_/¯ ¯\_(ツ)_/¯'
@@ -87,7 +87,7 @@ class TestLineParser(unittest.TestCase):
                   (Tokens.ID, 'x'), (Tokens.SHRUG,), (Tokens.SHRUG,),
                   (Tokens.SHRUG,), (Tokens.NUMBER, 4), (Tokens.ID, 'var'),
                   (Tokens.STRING, 'mm'), (Tokens.NUMBER, 434), (Tokens.EOL,)]
-        self.assertEqual(tokens, self.parser.parse(line))
+        self.assertEqual(tokens, self.parser.parse_line(line))
 
     def test_join_strings(self):
         # trivial
