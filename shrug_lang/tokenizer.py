6f896c4c9d01e4df08d517c8d2ab60a6897ec800
Benjamin Wu <benjaminwu124@gmail.com>
2018-09-21 12:01:53 -0400

    Move tokenizer functions back into class

--- shrug_lang/tokenizer.py
+++ shrug_lang/tokenizer.py
@@ -3,51 +3,51 @@ from typing import List
 from shrug_lang.shrug_token import Token, TokenType
 
 
-def parse_line(line: str):
-    unparsed_tokens = filter(None, join_strings(line.split(' ')))
-    tokens = [parse_token(unparsed) for unparsed in unparsed_tokens]
-    tokens.append(Token(TokenType.EOL))
-    return tokens
+class Tokenizer:
+    @staticmethod
+    def parse_line(line: str):
+        unparsed_tokens = filter(None, Tokenizer.join_strings(line.split(' ')))
+        tokens = [Tokenizer.parse_token(unparsed) for unparsed in unparsed_tokens]
+        tokens.append(Token(TokenType.EOL))
+        return tokens
 
-
-def join_strings(unparsed_tokens: List[str]):
-    """Join strings that have spaces in them"""
-    new_unparsed = []
-    reading_string = False
-    current_string = []
-    for unparsed_token in unparsed_tokens:
-        if reading_string:
-            if unparsed_token.endswith('"'):
-                reading_string = False
+    @staticmethod
+    def join_strings(unparsed_tokens: List[str]):
+        """Join strings that have spaces in them"""
+        new_unparsed = []
+        reading_string = False
+        current_string = []
+        for unparsed_token in unparsed_tokens:
+            if reading_string:
+                if unparsed_token.endswith('"'):
+                    reading_string = False
+                    current_string.append(unparsed_token)
+                    new_unparsed.append(' '.join(current_string))
+                    current_string.clear()
+                else:
+                    current_string.append(unparsed_token)
+            elif (unparsed_token.startswith('"') and
+                  (len(unparsed_token) == 1 or
+                   not unparsed_token.endswith('"'))):
+                reading_string = True
                 current_string.append(unparsed_token)
-                new_unparsed.append(' '.join(current_string))
-                current_string.clear()
             else:
-                current_string.append(unparsed_token)
-        elif (unparsed_token.startswith('"') and
-              (len(unparsed_token) == 1 or
-               not unparsed_token.endswith('"'))):
-            reading_string = True
-            current_string.append(unparsed_token)
-        else:
-            new_unparsed.append(unparsed_token)
-    if len(current_string):
-        new_unparsed.append(' '.join(current_string))
-    return new_unparsed
+                new_unparsed.append(unparsed_token)
+        if len(current_string):
+            new_unparsed.append(' '.join(current_string))
+        return new_unparsed
 
-
-def parse_token(unparsed_token: str):
-    """Get token from given string"""
-    if unparsed_token == '¯\_(ツ)_/¯':
-        return Token(TokenType.SHRUG)
-    if (len(unparsed_token) >= 2 and
-            unparsed_token.startswith('"') and
-            unparsed_token.endswith('"')):
-        return Token(TokenType.STRING, unparsed_token[1:-1])
-    if unparsed_token.isalpha():
-        return Token(TokenType.ID, unparsed_token)
-    if unparsed_token.isnumeric():
-        return Token(TokenType.NUMBER, int(unparsed_token))
-    return Token(TokenType.INVALID, unparsed_token)
-
-
+    @staticmethod
+    def parse_token(unparsed_token: str):
+        """Get token from given string"""
+        if unparsed_token == '¯\_(ツ)_/¯':
+            return Token(TokenType.SHRUG)
+        if (len(unparsed_token) >= 2 and
+                unparsed_token.startswith('"') and
+                unparsed_token.endswith('"')):
+            return Token(TokenType.STRING, unparsed_token[1:-1])
+        if unparsed_token.isalpha():
+            return Token(TokenType.ID, unparsed_token)
+        if unparsed_token.isnumeric():
+            return Token(TokenType.NUMBER, int(unparsed_token))
+        return Token(TokenType.INVALID, unparsed_token)
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,6 +1,6 @@
 import unittest
 
-from shrug_lang import tokenizer
+from shrug_lang.tokenizer import Tokenizer
 from shrug_lang.shrug_token import TokenType, Token
 
 
@@ -10,57 +10,57 @@ class TestTokenizer(unittest.TestCase):
     def test_empty_line(self):
         line = ''
         empty_line = [Token(TokenType.EOL)]
-        self.assertEqual(empty_line, tokenizer.parse_line(line))
+        self.assertEqual(empty_line, Tokenizer.parse_line(line))
 
         line = '     '
-        self.assertEqual(empty_line, tokenizer.parse_line(line))
+        self.assertEqual(empty_line, Tokenizer.parse_line(line))
 
     def test_shrug(self):
         line = '¯\_(ツ)_/¯'
         single_shrug = [Token(TokenType.SHRUG), Token(TokenType.EOL)]
-        self.assertEqual(single_shrug, tokenizer.parse_line(line))
+        self.assertEqual(single_shrug, Tokenizer.parse_line(line))
 
     def test_numbers(self):
         line = '2'
         single_number1 = [Token(TokenType.NUMBER, 2), Token(TokenType.EOL)]
-        self.assertEqual(single_number1, tokenizer.parse_line(line))
+        self.assertEqual(single_number1, Tokenizer.parse_line(line))
 
         line = '43435356'
         single_number2 = [Token(TokenType.NUMBER, 43435356), Token(TokenType.EOL)]
-        self.assertEqual(single_number2, tokenizer.parse_line(line))
+        self.assertEqual(single_number2, Tokenizer.parse_line(line))
 
         line = 'dfs3'
         invalid_id1 = [Token(TokenType.INVALID, 'dfs3'), Token(TokenType.EOL)]
-        self.assertEqual(invalid_id1, tokenizer.parse_line(line))
+        self.assertEqual(invalid_id1, Tokenizer.parse_line(line))
 
         line = 'abc%'
         invalid_id2 = [Token(TokenType.INVALID, 'abc%'), Token(TokenType.EOL)]
-        self.assertEqual(invalid_id2, tokenizer.parse_line(line))
+        self.assertEqual(invalid_id2, Tokenizer.parse_line(line))
 
     def test_ids(self):
         line = 'a'
         single_id1 = [Token(TokenType.ID, 'a'), Token(TokenType.EOL)]
-        self.assertEqual(single_id1, tokenizer.parse_line(line))
+        self.assertEqual(single_id1, Tokenizer.parse_line(line))
 
         line = 'variable'
         single_id2 = [Token(TokenType.ID, 'variable'), Token(TokenType.EOL)]
-        self.assertEqual(single_id2, tokenizer.parse_line(line))
+        self.assertEqual(single_id2, Tokenizer.parse_line(line))
 
     def test_strings(self):
         line = '""'
         empty_string = [Token(TokenType.STRING, ''), Token(TokenType.EOL)]
-        self.assertEqual(empty_string, tokenizer.parse_line(line))
+        self.assertEqual(empty_string, Tokenizer.parse_line(line))
 
         line = '"a"'
         short_string = [Token(TokenType.STRING, 'a'), Token(TokenType.EOL)]
-        self.assertEqual(short_string, tokenizer.parse_line(line))
+        self.assertEqual(short_string, Tokenizer.parse_line(line))
 
         line = '"a bc def gh i jk lmn op q rs tuv wx u y z"'
         long_string = [
             Token(TokenType.STRING, 'a bc def gh i jk lmn op q rs tuv wx u y z'),
             Token(TokenType.EOL)
         ]
-        self.assertEqual(long_string, tokenizer.parse_line(line))
+        self.assertEqual(long_string, Tokenizer.parse_line(line))
 
         line = '"fs\'d\' f" " s " " fd_ f"'
         weird_string = [
@@ -69,15 +69,15 @@ class TestTokenizer(unittest.TestCase):
             Token(TokenType.STRING, ' fd_ f'),
             Token(TokenType.EOL)
         ]
-        self.assertEqual(weird_string, tokenizer.parse_line(line))
+        self.assertEqual(weird_string, Tokenizer.parse_line(line))
 
         line = '"a'
         invalid_string = [Token(TokenType.INVALID, '"a'), Token(TokenType.EOL)]
-        self.assertEqual(invalid_string, tokenizer.parse_line(line))
+        self.assertEqual(invalid_string, Tokenizer.parse_line(line))
 
         line = '"¯\_(ツ)_/¯"'
         shrug_string = [Token(TokenType.STRING, '¯\_(ツ)_/¯'), Token(TokenType.EOL)]
-        self.assertEqual(shrug_string, tokenizer.parse_line(line))
+        self.assertEqual(shrug_string, Tokenizer.parse_line(line))
 
     def test_complex(self):
         line = ('¯\_(ツ)_/¯ n ¯\_(ツ)_/¯ x ¯\_(ツ)_/¯ ¯\_(ツ)_/¯ ¯\_(ツ)_/¯'
@@ -88,28 +88,28 @@ class TestTokenizer(unittest.TestCase):
                   Token(TokenType.SHRUG), Token(TokenType.NUMBER, 4),
                   Token(TokenType.ID, 'var'), Token(TokenType.STRING, 'mm'),
                   Token(TokenType.NUMBER, 434), Token(TokenType.EOL)]
-        self.assertEqual(tokens, tokenizer.parse_line(line))
+        self.assertEqual(tokens, Tokenizer.parse_line(line))
 
     def test_join_strings(self):
         # trivial
-        self.assertEqual([], tokenizer.join_strings([]))
-        self.assertEqual([''], tokenizer.join_strings(['']))
+        self.assertEqual([], Tokenizer.join_strings([]))
+        self.assertEqual([''], Tokenizer.join_strings(['']))
         # unfinished strings
-        self.assertEqual(['"'], tokenizer.join_strings(['"']))
-        self.assertEqual(['"affsdf'], tokenizer.join_strings(['"affsdf']))
+        self.assertEqual(['"'], Tokenizer.join_strings(['"']))
+        self.assertEqual(['"affsdf'], Tokenizer.join_strings(['"affsdf']))
         self.assertEqual(['"affsdf abc'],
-                         tokenizer.join_strings(['"affsdf', 'abc']))
+                         Tokenizer.join_strings(['"affsdf', 'abc']))
         # proper strings
-        self.assertEqual(['"abc"'], tokenizer.join_strings(['"abc"']))
+        self.assertEqual(['"abc"'], Tokenizer.join_strings(['"abc"']))
         self.assertEqual(['"abc "def ghi"'],
-                         tokenizer.join_strings(['"abc', '"def', 'ghi"']))
+                         Tokenizer.join_strings(['"abc', '"def', 'ghi"']))
         self.assertEqual(
             ['abc', '3', '"abc "def ghi"', 'hello', "ppp"],
-            tokenizer.join_strings(
+            Tokenizer.join_strings(
                 ['abc', '3', '"abc', '"def', 'ghi"', 'hello', "ppp"]))
         self.assertEqual(
             ['"     "'],
-            tokenizer.join_strings(['"', '', '', '', '', '"']))
+            Tokenizer.join_strings(['"', '', '', '', '', '"']))
 
 
 if __name__ == '__main__':
